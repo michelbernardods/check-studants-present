2321993b111436402d4751200c61c8c353bb4463
Michel Bernardo <michel_13s@outlook.com>
2020-08-26 19:31:31 -0300

    Alterando nome das variáves

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ let parse_matriculas = matriculas_cadastradas.split('\n');
 const alunos_comparecidos = fs.readFileSync('./files/alunos_comparecidos.txt', 'utf8')
 let alunos = alunos_comparecidos.split('\n');
 
-alunos = alunos.map(item => item.replace(" ",""))
-alunos = alunos.filter(item => item.startsWith('20') && item.length  == 10 )
+alunos = alunos.map(matricula_aluno => matricula_aluno.replace(" ","") && matricula_aluno.trim())
+alunos = alunos.filter(matricula_aluno => matricula_aluno.startsWith('20') && matricula_aluno.length  === 10 )
 
 
-let alunos_nao_presente = new Set(parse_matriculas);
+let alunos_nao_presente = new Set(parse_matriculas)
 for (let matriculas of alunos) {
    alunos_nao_presente.delete(matriculas)
 }
 
 
-alunos_nao_presente = Array.from(alunos_nao_presente);
-console.log(`Total de alunos que não compareceu na aula: ${alunos_nao_presente.length} \n\n  ${alunos_nao_presente} `);
+alunos_nao_presente = Array.from(alunos_nao_presente)
+console.log(`Total de alunos que não compareceu na aula: ${alunos_nao_presente.length} \n\n  ${alunos_nao_presente} `)
